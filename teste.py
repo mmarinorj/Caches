5fd95bc1e32919347ae3b5bc6eb01010464f7c0d
Marcelo Marino <mmarinorj@gmail.com>
2022-11-26 00:07:43 -0300

    att

--- teste.py
+++ teste.py
@@ -1,5 +1,16 @@
 import pandas as pd
 
-df = pd.read_excel("eventos.xlsx")
+# df = pd.read_excel("NOVA - Separação - Pessoal Eventos.xlsx")
+#
+# print(df.shape[0])
+# print(df)
+#
+df = pd.read_excel('NOVA - Separação - Pessoal Eventos.xlsx', None)
 
-print(df.shape[0])
+table = []
+for x in range(1, len(df)+1):
+    table.append(pd.read_excel('NOVA - Separação - Pessoal Eventos.xlsx', +str(x)))
+
+print(table)
+
+pd.read_excel()
